38ae1347f097dcdd00b2c9338623da7d22b32663
Devon Pirestani <dev@udel.edu>
2017-11-25 19:11:25 -0500

    japanese lotion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './japan.svg';
+import lotion from './fuckingpicture.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Wait how does the logo spin like that...</h1>
-        </header>
-        <p className="App-intro">
-          WHITE GIRLS ALL AROUND ME DOING COCAINE
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">
+              ろしゃんです
+            </h1>
+          </header>
+          <p className="App-intro">
+            おねがいします
+          </p>
+          <img src={lotion} className="Lotion-picture"/>
+        </div>
     );
   }
 }
