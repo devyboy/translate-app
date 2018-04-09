341cb571ce0ec2151489160860918bea4f90767e
Devon Pirestani <Devon@Devons-MacBook-Pro.local>
2018-04-08 22:02:08 -0400

    new flag, jap text, pulsing heart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './japan.svg';
-import lotion from './ineedtobemorecarefulwithmyfilenames.jpg';
+import japan from './images/japan.svg';
+import usa from './images/usa.svg';
+import heart from './images/heart.png';
+import lotion from './images/ineedtobemorecarefulwithmyfilenames.jpg';
 import './App.css';
 
 class App extends Component {
@@ -8,15 +10,17 @@ class App extends Component {
     return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={japan} className="App-logo" alt="japanese-flag" />
+              <img src={heart} className="heart" alt="heart" />
+            <img src={usa} id="amerikuh" className="App-logo" alt="american-flag" />
             <h1 className="App-title">
-              ろしゃんです
+              デラウェア大学へようこそ！
             </h1>
           </header>
           <p className="App-intro">
-            おねがいします
-          </p>
-          <img src={lotion} className="Lotion-picture"/>
+            友達の選考わかりません。選考はなんですか？お名前はなんですか？名前はデブンです。アメリカ人です。
+        </p>
+          <img src={lotion} className="Lotion-picture" alt="lotion"/>
         </div>
     );
   }
